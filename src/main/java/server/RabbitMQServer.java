9f14b2dc1c848b5d811573a63b4db38cc0576d63
GDLP8 <jorge.dasilva@alumni.mondragon.edu>
2022-05-10 11:18:32 +0200

    Dead-letter y threadpool de distro

---
+++ src/main/java/server/RabbitMQServer.java
@@ -0,0 +1,205 @@
+package server;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeoutException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
+public class RabbitMQServer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQServer.class);
+	final static String EXCHANGE_CAMERAS = "cameras";
+    final static String EXCHANGE_CLIENT = "distributor";
+    final static String DLX_EXCHANGE_NAME = "deadLetter";
+	final static String DLX_QUEUE_NAME = "deadLetterQueue";
+//	final static Integer THREAD_NUMBER = 2;
+    
+    ConnectionFactory factory, factory2;
+    ExecutorService executor, executor2;
+
+    public RabbitMQServer() {
+    	factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        
+        factory2 = new ConnectionFactory();
+        factory2.setHost("localhost");
+        factory2.setUsername("guest");
+        factory2.setPassword("guest");
+        
+        executor = Executors.newCachedThreadPool();
+        executor2 = Executors.newCachedThreadPool();
+    }
+    
+    public void suscribe() {
+
+        Channel channelCameras = null;
+        Channel channelClient = null;
+        try {
+        	
+        	Connection connection = factory.newConnection();
+        	Connection connection2 = factory2.newConnection();
+        	
+        	channelCameras = connection.createChannel();
+        	channelClient = connection2.createChannel();
+        	
+        	channelCameras.exchangeDeclare(EXCHANGE_CAMERAS, "fanout");
+        	channelClient.exchangeDeclare(EXCHANGE_CLIENT, "direct");
+        	channelClient.exchangeDeclare(DLX_EXCHANGE_NAME, "fanout");
+            
+			Map<String,Object> arguments = new HashMap<>();
+			arguments.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
+			
+			channelClient.queueDeclare(DLX_QUEUE_NAME, false, false,false,arguments);
+			channelClient.queueBind(DLX_QUEUE_NAME,DLX_EXCHANGE_NAME,"");
+
+            String queueName = channelCameras.queueDeclare().getQueue();
+            channelCameras.queueBind(queueName, EXCHANGE_CAMERAS, "");
+
+            ConsumerClient consumer = new ConsumerClient(channelClient, executor);
+            ConsumerCameras consumer2 = new ConsumerCameras(channelCameras, executor2);
+            boolean autoack = false;
+            String tag = channelClient.basicConsume(DLX_QUEUE_NAME, autoack, consumer);
+            String tag2 = channelCameras.basicConsume(queueName, autoack, consumer2);
+
+            LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
+            
+            synchronized (this) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            channelClient.basicCancel(tag);
+            channelCameras.basicCancel(tag2);
+            channelCameras.close();
+            channelClient.close();
+            connection.close();
+            connection2.close();
+            
+            executor.shutdown();
+            executor2.shutdown();
+    		try {
+    			executor.awaitTermination(200, TimeUnit.SECONDS);
+    			executor2.awaitTermination(200, TimeUnit.SECONDS);
+    		} catch (InterruptedException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+
+        } catch (IOException | TimeoutException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+    
+    public synchronized void stop() {
+        this.notify();
+    }
+    
+    public class ConsumerClient extends DefaultConsumer {
+
+    	ExecutorService executor;
+    	Channel channel;
+
+		public ConsumerClient(Channel channel, ExecutorService executor) {
+			super(channel);
+			this.executor = executor;
+			this.channel = channel;
+		}
+
+		@Override
+		public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+				throws IOException {
+			String message = new String(body, StandardCharsets.UTF_8);
+
+			try {
+                LOGGER.info(String.format("Received (channel %d) %s", channel.getChannelNumber(), new String(body)));
+
+                executor.submit(new Runnable() {
+                    public void run() {
+                        try {
+                            Thread.sleep(1000);
+                            LOGGER.info(String.format("Processed %s", message));
+                        } catch (InterruptedException e) {
+                            LOGGER.warn(String.format("Interrupted %s", message));
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                LOGGER.error("", e);
+            }	
+		}
+    }
+    
+    public class ConsumerCameras extends DefaultConsumer {
+
+		ExecutorService executor;
+    	Channel channel;
+
+		public ConsumerCameras(Channel channel, ExecutorService executor) {
+			super(channel);
+			this.executor = executor;
+			this.channel = channel;
+		}
+		
+		@Override
+		public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+				throws IOException {
+			String message = new String(body, StandardCharsets.UTF_8);
+
+			try {
+                LOGGER.info(String.format("Received (channel %d) %s", channel.getChannelNumber(), new String(body)));
+
+                executor.submit(new Runnable() {
+                    public void run() {
+                        try {
+                            Thread.sleep(1000);
+                            LOGGER.info(String.format("Processed %s", message));
+                        } catch (InterruptedException e) {
+                            LOGGER.warn(String.format("Interrupted %s", message));
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                LOGGER.error("", e);
+            }	
+		}
+    }
+    
+    public static void main(String[] args) {
+    	Scanner teclado = new Scanner(System.in);
+    	RabbitMQServer suscriber = new RabbitMQServer();
+    	Thread hiloEspera = new Thread(() -> {
+            teclado.nextLine();
+            suscriber.stop();
+            teclado.close();
+        });
+    	hiloEspera.start();
+        suscriber.suscribe();
+    }
+}
