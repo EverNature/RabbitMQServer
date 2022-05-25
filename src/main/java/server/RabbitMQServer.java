525998bfa5d258db4648dc950ccb4fc10259d2d9
GDLP8 <jorge.dasilva@alumni.mondragon.edu>
2022-05-25 17:23:44 +0200

    ack y nack con creación de ficheros

--- src/main/java/server/RabbitMQServer.java
+++ src/main/java/server/RabbitMQServer.java
@@ -1,15 +1,27 @@
 package server;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,16 +32,18 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.LongString;
 
 public class RabbitMQServer {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQServer.class);
 	final static String EXCHANGE_CAMERAS = "cameras";
-    final static String EXCHANGE_CLIENT = "distributor";
+	final static String QUEUE_CAMERAS = "queue_camera";
     final static String DLX_EXCHANGE_NAME = "deadLetter";
-	final static String DLX_QUEUE_NAME = "deadLetterQueue";
     
-    ConnectionFactory factory, factory2;
-    ExecutorService executor, executor2;
+    ConnectionFactory factory;
+    ExecutorService executor;
+    
+    List<String> invasiveSpecies;
 
     public RabbitMQServer() {
     	factory = new ConnectionFactory();
@@ -37,47 +51,32 @@ public class RabbitMQServer {
         factory.setUsername("guest");
         factory.setPassword("guest");
         
-        factory2 = new ConnectionFactory();
-        factory2.setHost("localhost");
-        factory2.setUsername("guest");
-        factory2.setPassword("guest");
-        
         executor = Executors.newCachedThreadPool();
-        executor2 = Executors.newCachedThreadPool();
+        invasiveSpecies = new ArrayList<>();
+        invasiveSpecies.add("AFRICAN OYSTER CATCHER");  // Esto hay que sustituirlo por llamada a base de datos y tal
     }
     
     public void suscribe() {
 
         Channel channelCameras = null;
-        Channel channelClient = null;
         try {
         	
         	Connection connection = factory.newConnection();
-        	Connection connection2 = factory2.newConnection();
-        	
         	channelCameras = connection.createChannel();
-        	channelClient = connection2.createChannel();
-        	
         	channelCameras.exchangeDeclare(EXCHANGE_CAMERAS, "fanout");
-        	channelClient.exchangeDeclare(EXCHANGE_CLIENT, "direct");
-        	channelClient.exchangeDeclare(DLX_EXCHANGE_NAME, "fanout");
-            
-			Map<String,Object> arguments = new HashMap<>();
+        	channelCameras.exchangeDeclare(DLX_EXCHANGE_NAME, "fanout");
+        	
+        	Map<String,Object> arguments = new HashMap<>();
 			arguments.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
-			
-			channelClient.queueDeclare(DLX_QUEUE_NAME, false, false,false,arguments);
-			channelClient.queueBind(DLX_QUEUE_NAME,DLX_EXCHANGE_NAME,"");
 
-            String queueName = channelCameras.queueDeclare().getQueue();
-            channelCameras.queueBind(queueName, EXCHANGE_CAMERAS, "");
+			channelCameras.queueDeclare(QUEUE_CAMERAS, false, false, false, arguments);
+            channelCameras.queueBind(QUEUE_CAMERAS, EXCHANGE_CAMERAS, "");
 
-            ConsumerClient consumer = new ConsumerClient(channelClient, executor);
-            ConsumerCameras consumer2 = new ConsumerCameras(channelCameras, executor2);
-            boolean autoack = true; //CAMBIAR ESTO A FALSE PARA DAR EL TRATAMIENTO QUE QUERAMOS (HAZ LO DE ACK NACK PERRO)
-            String tag = channelClient.basicConsume(DLX_QUEUE_NAME, autoack, consumer);
-            String tag2 = channelCameras.basicConsume(queueName, autoack, consumer2);
+            ConsumerCameras consumer = new ConsumerCameras(channelCameras, executor, invasiveSpecies);
+            boolean autoack = false;
+            String tag = channelCameras.basicConsume(QUEUE_CAMERAS, autoack, consumer);
 
-            LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
+            LOGGER.info(" [*] Waiting for messages. To exit press Enter");
             
             synchronized (this) {
                 try {
@@ -86,18 +85,13 @@ public class RabbitMQServer {
                     e.printStackTrace();
                 }
             }
-            channelClient.basicCancel(tag);
-            channelCameras.basicCancel(tag2);
+            channelCameras.basicCancel(tag);
             channelCameras.close();
-            channelClient.close();
             connection.close();
-            connection2.close();
             
             executor.shutdown();
-            executor2.shutdown();
     		try {
     			executor.awaitTermination(200, TimeUnit.SECONDS);
-    			executor2.awaitTermination(200, TimeUnit.SECONDS);
     		} catch (InterruptedException e) {
     			// TODO Auto-generated catch block
     			e.printStackTrace();
@@ -107,46 +101,10 @@ public class RabbitMQServer {
 
             e.printStackTrace();
         }
-
     }
     
     public synchronized void stop() {
         this.notify();
-    }
-    
-    public class ConsumerClient extends DefaultConsumer {
-
-    	ExecutorService executor;
-    	Channel channel;
-
-		public ConsumerClient(Channel channel, ExecutorService executor) {
-			super(channel);
-			this.executor = executor;
-			this.channel = channel;
-		}
-
-		@Override
-		public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
-				throws IOException {
-			String message = new String(body, StandardCharsets.UTF_8);
-
-			try {
-                LOGGER.info(String.format("Received (channel %d) %s", channel.getChannelNumber(), new String(body)));
-
-                executor.submit(new Runnable() {
-                    public void run() {
-                        try {
-                            Thread.sleep(1000);
-                            LOGGER.info(String.format("Processed %s", message));
-                        } catch (InterruptedException e) {
-                            LOGGER.warn(String.format("Interrupted %s", message));
-                        }
-                    }
-                });
-            } catch (Exception e) {
-                LOGGER.error("", e);
-            }	
-		}
     }
     
     public class ConsumerCameras extends DefaultConsumer {
@@ -154,11 +112,16 @@ public class RabbitMQServer {
 		ExecutorService executor;
     	Channel channel;
     	RESTClient cliente;
+    	final static String PHOTOS_FOLDER = "photos";
+        boolean reprocesar = false;
+		boolean multiple = false;
+		List<String> invasiveSpecies;
 
-		public ConsumerCameras(Channel channel, ExecutorService executor) {
+		public ConsumerCameras(Channel channel, ExecutorService executor, List<String> invasiveSpecies) {
 			super(channel);
 			this.executor = executor;
 			this.channel = channel;
+			this.invasiveSpecies = invasiveSpecies;
 	        
 	        cliente =new RESTClient();
 		}
@@ -173,26 +136,70 @@ public class RabbitMQServer {
                 
                 executor.submit(new Runnable() {
                     public void run() {
-                        byte[] photo = Base64.getDecoder().decode(message.getBytes());
-						Result response = cliente.sendPhotos(photo);
-						if(response == null) {
-							LOGGER.info(String.format("No valid response received"));
-						}
-						else {
-							if(response.getSegmented()) {
-								for(Prediction prediction:response.getPrediction()) {
-									LOGGER.info(String.format("Detected in image: %s", prediction.getClase()));
+						try {
+							MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+				            messageDigest.update(message.getBytes());
+				            
+				            String hash = ((LongString) properties.getHeaders().get("hash")).toString();
+				            
+				            if(hash.equals(new String(messageDigest.digest()))) {
+	                        	channel.basicAck(envelope.getDeliveryTag(), multiple);
+	                        	LOGGER.info(String.format("Image arrived correctly"));
+	                        }
+	                        else {
+	                        	channel.basicNack(envelope.getDeliveryTag(), multiple, reprocesar);
+	                        	LOGGER.info(String.format("Image suffered changes when since original"));
+	                        }
+				            
+							byte[] photo = Base64.getDecoder().decode(message.getBytes());
+	                        
+	                        InputStream is = new ByteArrayInputStream(photo);
+	                        BufferedImage newB = ImageIO.read(is);
+	                        
+	                        if(newB == null) {
+	                        	LOGGER.info(String.format("Unsupported format"));
+	                        	is.close();
+	                        }
+	                        else {
+	                        	is.close();
+								Result response = cliente.sendPhotos(photo);
+								if(response == null) {
+									LOGGER.info(String.format("No valid response received"));
 								}
-							}
-							else {
-								LOGGER.info(String.format("No animal detected in the image"));
-							}
+								else {
+									if(response.getSegmented()) {
+										int i = 0;
+										for(Prediction prediction:response.getPrediction()) {
+											LOGGER.info(String.format("Detected in image: %s", prediction.getClase()));
+											
+											InputStream is2 = new ByteArrayInputStream(Base64.getDecoder().decode(prediction.getImage().getBytes()));
+					                        BufferedImage newBi = ImageIO.read(is2);
+											String uuid = ((LongString) properties.getHeaders().get("uuid")).toString();
+											String filename = ((LongString) properties.getHeaders().get("filename")).toString();
+				                        	File file = new File(FileSystems.getDefault().getPath(PHOTOS_FOLDER).toString() + "\\" + uuid + "_" + filename + "_" + i + "_" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new java.util.Date()) + ".jpg");
+					                        ImageIO.write(newBi, "jpg", file);
+					                        is2.close();
+					                        i++;
+										}
+									}
+									else {
+										LOGGER.info(String.format("No animal detected in the image"));
+									}
+								}
+	                        }
+
+							//channel.basicAck(envelope.getDeliveryTag(), false);
+						} catch (IOException | NoSuchAlgorithmException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
-						
                     }
+                    
                 });
+
             } catch (Exception e) {
                 LOGGER.error("", e);
+                e.printStackTrace();
             }	
 		}
     }
